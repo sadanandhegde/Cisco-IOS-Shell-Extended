589621b96d99de3e8126587c83ef57c1c34461dd
sadanandhegde <sadanand.h@qualisystems.com>
2016-05-05 10:48:24 -0400

    Update __init__.py

--- cloudshell/networking/cisco/ios/extended/resource_drivers/__init__.py
+++ cloudshell/networking/cisco/ios/extended/resource_drivers/__init__.py
@@ -9,5 +9,5 @@ __path__ = extend_path(__path__, __name__)
 HandlerFactory.handler_classes['CATALYST_2950'] = CiscoIOSHandlerExtended
 HandlerFactory.handler_classes['CATALYST_3560'] = CiscoIOSHandlerExtended
 HandlerFactory.handler_classes['CATALYST_3850'] = CiscoIOSHandlerExtended
-HandlerFactory.handler_classes['IOS'] = CiscoIOSHandlerExtended
+HandlerFactory.handler_classes['IOS_EXT'] = CiscoIOSHandlerExtended
 HardwarePlatformDetector.RESOURCE_DRIVERS_MAP = CISCO_RESOURCE_DRIVERS_MAP
